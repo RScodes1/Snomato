d1d87a246f4a0395ed83950d0878e0c3f5ef4d0c
rajaSmartLifeHealth <raja.sekhar@smartlifehealth.co.uk>
2025-11-16 18:39:45 +0530

    cors error fix added even though same folder

--- Snomato-BE/index.js
+++ Snomato-BE/index.js
@@ -1,10 +1,20 @@
 const express = require('express');
 const { connection } = require('./config/db');
 const { userRouter } = require('./routes/user.routes');
- 
+ const cors = require('cors');
 require('dotenv').config();
 const port = process.env.PORT;
  const app = express();
+
+ app.use(
+  cors({
+    origin: ["https://snomato-jb2qvzvfl-rajasekhars-projects.vercel.app/login", "http://localhost:3000", "http://localhost:3001"],
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
+  })
+);
+
  app.get('/', async(req, res) => {
      res.send("hello");
  })
